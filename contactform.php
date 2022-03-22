25e44ef7d91df732d5ee419322fb736d6d33c1a9
Gearmo <93457954+Gearmo@users.noreply.github.com>
2022-03-21 19:44:31 -0700

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = "admin@mumbosmusicbox.com";
+
+    $email_subject = "Mumbo's Musicbox - New Message!";
+
+    $email_body = "User Name: $name. \n".
+        "User Email: $email.\n".
+            "User Message: $message.\n";
+
+$to = "admin@mumbosmusicbox.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header ("Location: 5 contact.html");
+?>
